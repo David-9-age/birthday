fa0864bf25fc6a61c38970dd0b28bf2cdbf2ecf0
David-9-age <davidumea20@gmail.com>
2025-12-15 15:51:06 +0100

    Add files via upload

---
+++ index.py
@@ -0,0 +1,7 @@
+birthday1 = "2000-01-01"
+birthday2 = "1998-05-12"
+birthday3 = "2001-07-23"
+birthday4 = "1995-09-15"
+birthday5 = "1999-11-30"
+
+print(birthday1, birthday2, birthday3, birthday4, birthday5, sep="\n")
